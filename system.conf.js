71147ddda7596ba1b4358def151a81eb3e597c58
Sam Schneider <sam.schneider@igtm.com>
2016-10-27 08:41:05 -0500

    Updated system.conf.js to get Karma running.

--- system.conf.js
+++ system.conf.js
@@ -4,7 +4,7 @@ System.config({
   transpiler: "traceur",
   paths: {
     "systemjs": "node_modules/systemjs/dist/system.js",
-    "traceur": "node_modules/traceur/dist/commonjs/traceur.min.js",
+    "traceur": "node_modules/traceur/bin/traceur.js",
     "github:*": "src/github/*"
   }
 });
