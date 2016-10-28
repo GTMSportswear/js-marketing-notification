7ee4bad9ea15d6497d835726e61df0758d89fa5a
Chris Young <someyoungideas@gmail.com>
2016-10-28 10:20:11 -0500

    adding js-local-storage-manager module

--- system.conf.js
+++ system.conf.js
@@ -5,6 +5,10 @@ System.config({
   paths: {
     "systemjs": "node_modules/systemjs/dist/system.js",
     "traceur": "node_modules/traceur/bin/traceur.js",
-    "github:*": "src/github/*"
+    "github:*": "/github/*"
+  },
+
+  map: {
+    "gtmsportswear/js-local-storage-manager": "github:gtmsportswear/js-local-storage-manager@1.0.2"
   }
 });
